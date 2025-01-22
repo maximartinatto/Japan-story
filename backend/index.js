782953c032819c5023a4de3d1688fa95ac54db4b
maximartinatto <martinattomaximiliano@gmail.com>
2025-01-22 09:08:58 -0300

    revisar postman

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,11 @@ const express = require("express");
 const cors = require("cors")
 const jwt = require("jsonwebtoken");
 
-const User = require("./models/user.model")
+const { authenticateToken } = require("./utilities");
+
+const User = require("./models/user.model");
+const JapanStory = require("./models/japanStory.model")
+
 
 
 mongoose.connect(config.connectionString);
@@ -61,7 +65,7 @@ app.post("/create-account", async (req, res ) => {
 });
 
 // Login
-app.post("/", async (req, res) => {
+app.post("/login", async (req, res) => {
     const { email, password } = req.body;
 
     if(!email || !password) {
@@ -72,7 +76,75 @@ app.post("/", async (req, res) => {
     if (!user) {
         return res.status(400).json({ message: "User not found" });
     }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid){
+        return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const accessToken = jwt.sign(
+        { userId: user._id},
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: "72h"
+        }
+    );
+
+    return res.json({
+        error: false,
+        message: "Login Successfully",
+        user: { fullName: user.fullName, email: user.email },
+        accessToken
+    });
 })
+
+// Get user
+app.get("/get-user", authenticateToken, async (req, res) => {
+    const { userId } = req.user
+
+    const isUser = await User.findOne({ _id: userId });
+
+    if (!isUser) {
+        return res.sendStatus(401);
+    }
+
+    return res.json({
+        user: isUser,
+        message: ""
+    });
+})
+
+// Add japan story
+
+app.post("/add-japan-story", authenticateToken, async (req, res) => {
+    const { title, story, visitedLocation, imageUrl, visitedDate } = req.body;
+    const { userId } = req.user
+
+    // validate required fields
+    if (!title || !story || !visitedLocation || !imageUrl || !visitedDate) {
+        return res.status(400).json({ error: true, message: "All fields are required" });
+    }
+
+    // Convert visitedDate from milliseconds to Date object
+    const parsedVisitedDate = new Date(parseInt(visitedDate));
+
+    try {
+        const japanStory = new JapanStory({
+            title,
+            story,
+            visitedLocation,
+            userId,
+            imageUrl,
+            visitedDate: parsedVisitedDate
+        });
+
+        await japanStory.save();
+        res.status(200).json({ story: japanStory, message: "Added Successfully" });
+    } catch (error) {
+        res.status(400).json({ error: true, message: error.message });
+    }
+})
+
 
 app.listen(3001);
 module.exports = app;
